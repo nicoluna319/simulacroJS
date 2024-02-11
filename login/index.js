aa7573fe1632198998722cc43a2533172a82a719
Nicolas Luna Romero <134241117+elbazingaxd@users.noreply.github.com>
2024-02-10 19:32:22 -0500

    1act

---
+++ login/index.js
@@ -0,0 +1,16 @@
+//Selectores
+const formLogin = document.getElementById("formLogin");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+//Eventos
+
+formLogin.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    logIn()
+})
+
+async function logIn(){
+
+}
